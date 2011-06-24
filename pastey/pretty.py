88f7bc1b0e03f3f7c9c337f34621f2af3cb0152b
Caleb Smith <csmith@caktusgroup.com>
2011-06-24 14:08:14 -0400

    Improved comments and moved tersify function to the pretty.py module

--- pastey/pretty.py
+++ pastey/pretty.py
@@ -5,17 +5,23 @@ from pygments.styles import get_style_by_name
 
 from pastey.models import Style
 
-def pretty_pastes(paste_list):
-    """
-    """
-    style_choice = Style()  
-    css_styles = []
+DEFAULT_STYLE = 'colorful'
 
-    for paste in paste_list:
-        paste.code_paste, temp_style = pretty_print(paste, style_choice)
-        css_styles.append(temp_style)
-
-    return zip(paste_list, css_styles)
+def tersify(pastes, charlimit=400):
+    """returns a list of Code objects with a truncated code field.
+    
+    The function takes a list of Code objects as its first argument and returns
+    this list. Thee character limit defaults to 400 and can be set by calling 
+    the function with a second argument. 
+    
+    The truncated text will contain ... concatenated at the end.
+    """
+    
+    for paste in pastes:
+        if len(paste.code_paste) > charlimit or charlimit == 0:
+            paste.code_paste = paste.code_paste[:charlimit]
+            paste.code_paste += '...'
+    return pastes
 
 def pretty_print(paste, style_choice):	
     """Use Pygments library to highlight a TextField       
@@ -24,7 +30,7 @@ def pretty_print(paste, style_choice):
         where str1 is the formatted text, and str2 is the css style block
         
     Syntax:
-        str, str = pretty_print(str)
+        code_str, style_str = pretty_print(code_str)
         
     Ex:
         Views:
@@ -36,11 +42,30 @@ def pretty_print(paste, style_choice):
     """
     if paste.language:lexer = get_lexer_by_name(paste.language, stripall=True)
     if not paste.language: lexer = guess_lexer(paste.code_paste, stripall=True)		
-    if not style_choice.highlight: style_choice.highlight = 'colorful'	
+    if not style_choice.highlight: style_choice.highlight = DEFAULT_STYLE	
 
     formatter = HtmlFormatter(linenos="table", cssclass="source", style= style_choice.highlight)
     result = highlight(paste.code_paste, lexer, formatter)		
     css_style = formatter.get_style_defs()		
     
     return result, css_style
+
+def pretty_pastes(paste_list):
+    """Applies pretty_print to every object in a list
+    
+    Use: paste_list = pretty_pastes(paste_list)
+    where paste_list contains the Code objects to be listed
+    
+    The function returns a list of two tuples in which the first variable 
+    contains the paste object while the second contains the style text to be 
+    used in the template. The style used is the default value DEFAULT_STYLE.
+    """
+    style_choice = Style()  
+    css_styles = []
+
+    for paste in paste_list:
+        paste.code_paste, temp_style = pretty_print(paste, style_choice)
+        css_styles.append(temp_style)
+
+    return zip(paste_list, css_styles)
 
