0210b6ce7f500694d2945fe33aa75aba34afe9f7
Caleb Smith <csmith@caktusgroup.com>
2011-07-13 15:21:07 -0400

    PDF creation is now delegated to a celery task to prevent performance issues

--- pastey/tasks.py
+++ pastey/tasks.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta
+import cStringIO as StringIO
+from cgi import escape
+
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
 
 from celery.task import task
 from celery.decorators import periodic_task
 
+import ho.pisa as pisa
+
 from pastey.models import Code, CodeForm
 
-    
-@periodic_task(run_every=timedelta(seconds=120))
+
+@periodic_task(run_every=timedelta(seconds=900))
 def find_old():
     delete_sum = 0
     paste_list = Code.objects.all()
@@ -26,3 +34,11 @@ def delete_old(paste):
     paste.delete()
     return 
     
+@task
+def create_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = StringIO.StringIO()
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+    return pdf, result
